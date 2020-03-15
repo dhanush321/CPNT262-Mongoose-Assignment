995a4696033ccb5cf9e902f23fd84671af6d4af9
Dhanush Krishnan <44211841+dhanush321@users.noreply.github.com>
2020-03-15 13:15:20 -0600

    Update Images.js

--- models/Images.js
+++ models/Images.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 // Step 1: Define our Schema
-// See: https://mongoosejs.com/docs/guide.html
 /*
 "Everything in Mongoose starts with a Schema. Each schema maps to a MongoDB collection and defines the shape of the documents within that collection."
 */
@@ -20,9 +19,5 @@ const imageSchema = new mongoose.Schema(
 );
 
 // Compile and export our model using the above Schema.
-// See: https://mongoosejs.com/docs/models.html 
 
 module.exports = mongoose.model('Image', imageSchema);
-// Important: The first argument of mongoose.model() is the singular name of the collection your model is for. 
-// ** Mongoose automatically looks for the plural, lowercased version of your model name. **"
-// In our example, we name our model 'Definition' and mongoose will automatically look for the collection 'definitions'
